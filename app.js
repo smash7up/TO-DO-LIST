ec8b4e355f19d4a865bb804cfccbccf426e961a8
loicWCS <loic.lebon.wcs@gmail.com>
2022-05-27 01:38:09 +0200

    effacement du texte quand on clic sur le boutton +

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const formulaire = document.getElementsByTagName('form');
-const text = document.getElementById('myText').value;
-const add = document.getElementById('ajout');
-const hide = document.createElement('input');
-hide.setAttribute('supprimer', 'X');
-const check = document.getElementsByClassName('check');
-const list = document.querySelector('ul');
+const formulaire = document.getElementsByTagName("form");
+const text = document.getElementById("myText").value;
+const add = document.getElementById("ajout");
+// const hide = document.createElement("input");
+// hide.setAttribute("supprimer", "X");
+const check = document.getElementsByClassName("check");
+const list = document.querySelector("ul");
 
 function addList() {
-    const newLi = document.createElement('li');
-    const newCheck = document.createElement('input');
-    newCheck.type = "checkbox";
-    list.appendChild(newLi);
-    newLi.textContent = myText.value;
-    newLi.appendChild(newCheck);
-    console.log(newLi.textContent);
+  const newLi = document.createElement("li");
+  const newCheck = document.createElement("input");
+  newLi.classList.add('myLi');
+  newCheck.type = "checkbox";
+  list.appendChild(newLi);
+  newLi.textContent = myText.value;
+  newLi.appendChild(newCheck);
+  // reset la valeur dans l'input au click sur le "+"
+  myText.value = "";
 }
+
